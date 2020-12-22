bf3d9da43a3a68be07a3746e13324b6fdef230cd
August Duet <august.duet@revature.com>
2020-12-22 16:10:38 -0500

    add rest controller

--- src/main/java/com/ex/devopsapp/DevopsAppApplication.java
+++ src/main/java/com/ex/devopsapp/DevopsAppApplication.java
@@ -2,12 +2,22 @@ package com.ex.devopsapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/app")
 public class DevopsAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DevopsAppApplication.class, args);
+    }
+
+    @GetMapping("/hi")
+    public String sayHi() {
+        return "hi";
     }
 
 }
